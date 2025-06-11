e704817aaba29473be3338d3f5a92b7a294ff2a2
mukti2216patel <mukti2216patel@gmail.com>
2025-06-11 21:59:49 +0530

    null

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className="min-h-screen bg-black text-white font-poppins flex items-center justify-center">
-      <h1 className="text-4xl font-lexend">
+      <h1 className="text-4xl bg-gray-400 text-blue">
         Tailwind + React + Vite + Google Fonts is working!
       </h1>
     </div>
