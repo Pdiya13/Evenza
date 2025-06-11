164aaa8e201eb84b4f20f9f78e08d42c09e0318d
mukti2216patel <mukti2216patel@gmail.com>
2025-06-11 22:15:12 +0530

    adding intro

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 
 import './App.css'
+import Intro from './components/Intro'
 
 function App() {
- 
-
   return (
     <>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Intro />
     </>
   )
 }
