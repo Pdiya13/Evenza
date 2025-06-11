860c614dd4407a03e4dc388886530164a7086996
Pdiya13 <pdiya2945@gmail.com>
2025-06-12 01:22:03 +0530

    change in features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import Features from './components/Features/Features'
+import Footer from './components/Footer'
 import Intro from './components/Intro'
 
 function App() {
   return (
     <>
       <Intro />
+      <Features/>
+      <Footer/>
     </>
   )
 }
--- client/src/components/Features/Features.jsx
+++ client/src/components/Features/Features.jsx
@@ -41,26 +41,34 @@ function Features() {
         <Card
           title="Custom Event Planning"
           description="Tailored planning for weddings, parties, and corporate events."
+        className="bg-gradient-to-r from-teal-400 to-cyan-600"
         />
         <Card
           title="Creative Design"
           description="Innovative themes and decoration ideas to wow your guests."
+          className="bg-gradient-to-br from-green-200 to-blue-500"
         />
         <Card
           title="Venue Management"
           description="We handle bookings, layout, and venue logistics."
+          className="bg-gradient-to-tr from-teal-400 to-blue-600"
         />
         <Card
           title="Entertainment Setup"
           description="From DJs to live performers, we handle all entertainment."
+       className=" bg-gradient-to-r from-emerald-400 to-sky-500"
+
+
         />
         <Card
           title="Catering Coordination"
           description="Delicious menus designed and managed to fit your theme."
+          className="bg-gradient-to-bl from-lime-300 to-sky-400"
         />
         <Card
           title="On-site Coordination"
           description="Our team ensures everything runs smoothly on the event day."
+          className="bg-gradient-to-r from-sky-500 to-green-500"
         />
       </div>
 
