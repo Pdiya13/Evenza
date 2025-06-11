4a074e815ade358fce9d09ab1c924c85bf376357
mukti2216patel <mukti2216patel@gmail.com>
2025-06-11 22:25:34 +0530

    null

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,6 @@ import Intro from './components/Intro'
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
       <Intro />
     </>
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
 function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+           
+            <nav class="flex justify-between items-center bg-black p-4 text-white">
+                
+                <div class="font-bold text-lg">My Brand</div>
+                
+                <ul class="flex space-x-4">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
 
 export default Header
