430d2bbef4dd05d2ec0e9ac357841f075cffaab5
mukti2216patel <mukti2216patel@gmail.com>
2025-06-12 00:15:00 +0530

    add img and header for scrolling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Header() {
     return (
         <div>
-            <nav class="flex justify-between items-center bg-black p-4 text-white">
-                <div class="font-bold text-2xl pl-12">EVENZA</div>
-                <ul class="flex">
+            <nav className="flex justify-between items-center bg-black p-4 text-white">
+                <div className="font-bold text-2xl pl-12 font-lexend-giga-custom">EVENZA</div>
+                <ul className="flex font-lexend-giga-custom">
                     <li className='pr-16 font-semibold' >Home</li>
                     <li className='pr-16 font-semibold'>About</li>
                     <li className='pr-16 font-semibold'>Services</li>
--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
+import React, { useState, useEffect, useRef } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import img1 from '../images/img1.jpg';
+import img2 from '../images/img2.jpg';
+import img3 from '../images/img3.jpg';
+import img4 from '../images/img4.jpg';
 
 function Intro() {
-  return (
-    <div className='bg-black w-screen h-screen'>
-      <Header />
-      <Footer />
-    </div>
-  )
+    const images = [img1, img2, img3, img4];
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const scrollRef = useRef(null);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex(prev => (prev + 1) % images.length)
+        }, 2000);
+        return () => clearInterval(interval);
+    });
+
+    useEffect(() => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollTo({
+                left: window.innerWidth * currentIndex,
+                behavior: 'smooth',
+            });
+        }
+    }, [currentIndex]);
+
+    return (
+        <div className="bg-black text-white min-h-screen flex flex-col overflow-hidden">
+            <main className="w-screen h-screen">
+                <div className="sticky top-0 z-50 bg-black">
+                    <Header />
+                </div>
+
+                <div
+                    ref={scrollRef}
+                    className="flex flex-row overflow-x-auto overflow-y-hidden snap-x snap-mandatory scroll-smooth"
+                >
+                    {images.map((img, index) => (
+                        <div
+                            key={index}
+                            className="snap-start min-w-screen flex justify-center"
+                        >
+                            <img src={img} alt={`img${index + 1}`} className="object-contain rounded" />
+                        </div>
+                    ))}
+                </div>
+            </main>
+
+            <Footer />
+        </div>
+    );
 }
 
-export default Intro
+export default Intro;
