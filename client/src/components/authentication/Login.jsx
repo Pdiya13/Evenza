476d989da2036da006f49e75cf90239ce6efc2e0
mukti2216patel <mukti2216patel@gmail.com>
2025-06-14 16:39:11 +0530

    adding login page

--- client/src/components/authentication/Login.jsx
+++ client/src/components/authentication/Login.jsx
@@ -1,16 +1,71 @@
-import React from 'react'
-import Signup from './Signup'
-import {Link} from 'react-router-dom'
-function Login() {
-    return (
-        <div>
-            <Link to="/Signup">
-                <button>
-                    get  started
-                </button>
-            </Link>
-        </div>
-    )
-}
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-export default Login
+export default function Signup() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    role: 'user',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Signup Data:', formData);
+    // Submit form logic here
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center   bg-gradient-to-br from-black via-gray-800 via-black to-gray-900">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-gradient-to-br from-gray-800  to-black text-white px-8 py-10 rounded-xl shadow-xl w-full max-w-md"
+      >
+        <h2 className="text-3xl font-semibold mb-6 text-center tracking-wide">LOGIN</h2>
+
+      
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          className="w-full mb-4 px-4 py-2 bg-gray-800 text-white rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          className="w-full mb-6 px-4 py-2 bg-gray-800 text-white rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+
+        <button
+          type="submit"
+          className="w-full bg-slate-700 hover:bg-slate-800 transition duration-300 font-bold py-2 rounded"
+        >
+          Login
+        </button>
+
+         <p className="mt-6 text-center text-gray-400">
+            Don't have an account?{' '}
+            <Link to="/signup" className="text-blue-500 hover:underline font-semibold">
+                SignUp
+            </Link>
+        </p>
+      </form>
+    </div>
+  );
+}
