bc8af80f7f83a34a8f67fad1397ac7b9d80b959a
mukti2216patel <mukti2216patel@gmail.com>
2025-06-16 20:26:07 +0530

    creating dashboard with upcoming and ongoing events with analytics

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,10 +14,10 @@ import {
 
 function Navbar() {
     return (
-        <div className="flex items-center h-[85vh]">
+        <div className="flex items-center h-[100vh]">
 
             <div className="group w-18 hover:w-52 transition-[width] duration-300 h-full bg-[#0D1117] mt-8 rounded-lg px-4 py-10 overflow-hidden">
-                {/* <div className="font-bold text-2xl pl-12 font-lexend-giga-custom text-[#C3D0E5]">EVENZA</div> */}
+
                 <ul className="text-white flex flex-col justify-center gap-8 text-base font-normal h-full">
                     <li className="hover:bg-[#F4F4F4] px-2 py-3 rounded-md hover:text-[#0D1117] flex items-center gap-3">
                         <div className="min-w-[20px] text-xl"><FaTachometerAlt /></div>
