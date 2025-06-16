ce84d7b79ac5c9de27fc12329429eb68e39f5ade
mukti2216patel <mukti2216patel@gmail.com>
2025-06-16 16:45:57 +0530

    adding vendors in navbar

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,8 +3,9 @@ import Navbar from './Navbar'
 import Header from './Header'
 function Dashboard() {
   return (
-    <div className='w-screen min-h-screen bg-white'>
+    <div className='w-screen min-h-screen bg-[#F1F5FF]'>
         <Header title={["Reminders" , "User"]}/>
+        
         <Navbar />
     </div>
   )
