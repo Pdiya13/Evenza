87206e65e55a23aa1f295016fc1e66c79ea721bd
mukti2216patel <mukti2216patel@gmail.com>
2025-06-21 16:54:16 +0530

    null

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connectDB =( async ()=>{
+const connectDB = async ()=>{
     try{
     const conn = await mongoose.connect(process.env.MONGODB_URL);
     console.log("DB connected");
@@ -10,6 +10,6 @@ const connectDB =( async ()=>{
     {
         console.log(error);
     }
-})
+}
 
 module.exports = connectDB;
