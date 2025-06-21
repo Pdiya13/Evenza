7cf73044e821c5e857fde02d501467a292a776f0
mukti2216patel <mukti2216patel@gmail.com>
2025-06-21 15:22:06 +0530

    adding user Schema

---
+++ server/src/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+    name:{
+        type:String , required:true
+    },
+    email:{
+        type:String , required:true , unique:true
+    },
+    password:{
+        type:String , 
+        required:true,
+    },
+    role:{
+        type:String,
+        enum:['user' , 'vendor'],
+        default : 'user'
+    }
+})
+
+const userModel = mongoose.model('userModel' , user);
+
+module.exports = {userModel};
